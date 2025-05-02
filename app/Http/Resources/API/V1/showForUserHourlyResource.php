777847f24efc4e4c6fda71da4b5c455e122f4081
helalscript <husoaib422@gmail.com>
2025-05-02 15:24:15 +0600

    Fix casing for resource class names in UserHourly and UserDashboardTransaction resources

--- app/Http/Resources/API/V1/showForUserHourlyResource.php
+++ app/Http/Resources/API/V1/showForUserHourlyResource.php
@@ -5,7 +5,7 @@ namespace App\Http\Resources\API\V1;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class showForUserHourlyResource extends JsonResource
+class ShowForUserHourlyResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
