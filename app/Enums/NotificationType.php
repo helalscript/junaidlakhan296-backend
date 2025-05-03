7b1c38cc676a76a490e9d1db7c79ff1149f843a7
helalscript <husoaib422@gmail.com>
2025-05-03 20:42:35 +0600

    Refactor NotificationType enum to standardize notification case names and improve clarity

--- app/Enums/NotificationType.php
+++ app/Enums/NotificationType.php
@@ -4,18 +4,20 @@ namespace App\Enums;
 
 enum NotificationType: string
 {
-    case BookingConfirmation = 'booking_confirmation';
-    case BookingReminder = 'booking_reminder';
-    case BookingCancelled = 'booking_cancelled';
-    case PaymentReceived = 'payment_received';
-    case NewMessageReceived = 'new_message_received';
-    case NewBookingReceived = 'new_booking_received';
-    case PayoutProcessed = 'payout_processed';
-    case OthersNotification = 'others_notification';
-    case ContactSupport = 'contact_support';
-    case InfoNotification = 'info_notification';
-    case InfoNotificationWithQrCode = 'info_notification_with_qrcode';
-    case Paymnet = 'payment';
+    case BookingConfirmationNotification = 'booking_confirmation';
+    case BookingReminderNotification = 'booking_reminder';
+    case BookingCancelledNotification = 'booking_cancelled';
+    case PaymentInformationNotification = 'payment_information';
+    case MessageInformationNotification = 'message_information';
+    case OthersNotificationNotification = 'others_notification';
+    case NewReservationReceivedNotification = 'new_reservation_received';
+    case ReservationReminderNotification = 'reservation_reminder';
+    case ReservationCompletedNotification = 'reservation_completed';
+    case ReservationCancelledNotification = 'reservation_cancelled';
+    case ExtensionReservationReceivedNotification = 'extension_reservation_received';
+    //other notifications
+    case ContactSupportNotification = 'contact_support';
+    case InfoNotificationNotification = 'info_notification';
+    case InfoNotificationWithQrCodeNotification = 'info_notification_with_qrcode';
     
-
 }
