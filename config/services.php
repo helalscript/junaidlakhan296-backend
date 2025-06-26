a9ff0d82236c608f5f4a78f376284bd041d2b007
helalscript <husoaib422@gmail.com>
2025-06-26 12:14:38 +0600

    social login added

--- config/services.php
+++ config/services.php
@@ -39,5 +39,10 @@ return [
         'secret' => env('STRIPE_SECRET_KEY'),
         'webhook_secret' => env('STRIPE_WEBHOOK_SECRET'),
     ],
+    'google' => [
+        'client_id' => env('GOOGLE_CLIENT_ID'),
+        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+        'redirect' => env('GOOGLE_REDIRECT_URI'),
+    ],
 
 ];
